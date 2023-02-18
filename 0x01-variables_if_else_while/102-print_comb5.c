8bdd99179bcaa76b1f563d18ff87b31f95119cd7
mina <longterm15@gmail.com>
2023-02-17 17:46:56 -0800

    another drama

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,6 +11,7 @@ int main(void)
 	int x;
 	int y;
 	int k;
+	int n = 0;
 
 	for (y = 48; y < 58; y++)
 	{
@@ -19,15 +20,13 @@ int main(void)
 	for (x = 48; x < 58; x++)
 	{
 	for (i = 48; i < 58; i++)
-	{
-		if (i != 48 && x != 48 && y != 48 && k != 48)
-		{
-		putchar(y);
-		putchar(k);
+	{	
+		putchar(y + n);
+		putchar(k + n);
 		putchar(' ');
-		putchar(x);
-		putchar(i);
-		}
+		putchar(x + n);
+		putchar(i + n);
+	
 		if (i == 57 && x == 57 && y == 57 && k == 57)
 	       { putchar('\n');
 	       }
