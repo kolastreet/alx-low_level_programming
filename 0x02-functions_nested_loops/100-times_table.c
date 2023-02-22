ece8e841f9a0687f38179c2726abe4580b13de0e
mina <longterm15@gmail.com>
2023-02-21 17:16:31 -0800

    table new

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,43 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_times_table - Prints the n times table
- *
+ * for all number from 0 to 15
  * @n: number times table (0 < n <= 15)
  *
  * Return: no return
  */
 void print_times_table(int n)
 {
-	int a, b, op;
+	int x, y, multi;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (a = 0; a <= n; a++)
+		for (x = 0; x <= n; x++)
 		{
 			_putchar(48);
-			for (b = 1; b <= n; b++)
+			for (y = 1; y <= n; y++)
 			{
-				op = a * b;
+				multi = x * y;
 				_putchar(44);
 				_putchar(32);
-				if (op <= 9)
+				if (multi <= 9)
 				{
 					_putchar(32);
 					_putchar(32);
-					_putchar(op + 48);
+					_putchar(multi + 48);
 				}
-				else if (op <= 99)
+				else if (multi <= 99)
 				{
 					_putchar(32);
-					_putchar((op / 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar((multi / 10) + 48);
+					_putchar((multi % 10) + 48);
 				}
 				else
 				{
-					_putchar(((op / 100) % 10) + 48);
-					_putchar(((op / 10) % 10) + 48);
-					_putchar((op % 10) + 48);
+					_putchar(((multi / 100) % 10) + 48);
+					_putchar(((multi / 10) % 10) + 48);
+					_putchar((multi % 10) + 48);
 				}
 			}
 			_putchar('\n');
