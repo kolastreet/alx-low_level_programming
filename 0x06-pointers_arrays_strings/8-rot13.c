0d302e4316c7f6956955b5ff997c20d1fb8551fa
mina <longterm15@gmail.com>
2023-03-02 15:51:00 -0800

    rot

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string using rot13
+ * @s: input string.
+ * Return: the pointer to dest.
+ */
+
+char *rot13(char *s)
+{
+	int count = 0, l;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char rot13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+	while (*(s + count) != '\0')
+	{
+		for (l = 0; l < 52; l++)
+		{
+			if (*(s + count) == alphabet[l])
+			{
+				*(s + count) = rot13[l];
+				break;
+			}
+		}
+		count++;
+	}
+
+	return (s);
+}
